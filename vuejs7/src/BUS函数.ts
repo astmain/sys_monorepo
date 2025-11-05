a233a5d603fbd4546204368a5a6a839feb8d20a3
astmain <1311192345@qq.com>
2025-11-05 23:33:57 +0800

    1

--- vuejs7/src/BUS函数.ts
+++
@@ -1,8 +0,0 @@
-// import { ref, computed } from 'vue';
-import { defineStore } from "pinia"
-
-export const use_BUS = defineStore("localStorage_BUS", () => {
-  const state = {}
-
-  return { state }
-})
