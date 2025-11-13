a82751f2b624cb4155060828e2472935e7b67d0c
astmain <1311192345@qq.com>
2025-11-13 16:31:43 +0800

    1

--- tool_orm_Drizzle1/src/schema/tb_account.ts
+++ tool_orm_Drizzle1/src/schema/tb_account.ts
@@ -1,11 +1,11 @@
 import { pgTable, serial, timestamp, varchar } from "drizzle-orm/pg-core"
+import { id_str } from "../common"
 
 
 // tb_account 表
 export const tb_account = pgTable("tb_account", {
-    id: serial("id").primaryKey(),
+    id: id_str,
     name: varchar("name", { length: 255 }).default("小许"),
-    at_created: timestamp('at_created', { withTimezone: false }).defaultNow().notNull(),
-    at_updated: timestamp('at_updated', { withTimezone: false }).defaultNow().notNull(),
+
 })
 
--- tool_orm_Drizzle1/src/schema/tb_test1.ts
+++ tool_orm_Drizzle1/src/schema/tb_test1.ts
@@ -1,4 +1,5 @@
 import { pgTable, serial, varchar } from "drizzle-orm/pg-core"
+import { id_str, at_created, at_updated } from "../common"
 
 // 1. tb_test1 表
 export const tb_test1 = pgTable("tb_test1", {
